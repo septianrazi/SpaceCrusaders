3dbfcb7980564891017459b2d0f5e26713349efd
Kriti Tripathi <u6058693@anu.edu.au>
2018-04-19 16:41:21 +1000

    Added global variables

---
+++ app/src/main/java/com/example/comp2100/retrogame2018s1/GlobalGameVariables.java
@@ -0,0 +1,11 @@
+package com.example.comp2100.retrogame2018s1;
+
+/**
+ * Created by Jasper McNiven on 19/04/2018.
+ * This class contains a list of variables which are used throughout the various game classes
+ */
+
+public class GlobalGameVariables {
+    public static float gravity = 1.0f; // The speed at which shit falls
+    public static float gameSpeed = 2.0f; // The speed at which the screen scrolls
+}
