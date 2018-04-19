11e801d16fced2d4d6368e56f045acd6c94a4e92
Kriti Tripathi <u6058693@anu.edu.au>
2018-04-19 16:24:36 +1000

    Created the base GameObject class and supporting class Bounds.

---
+++ app/src/main/java/com/example/comp2100/retrogame2018s1/Bounds.java
@@ -0,0 +1,45 @@
+package com.example.comp2100.retrogame2018s1;
+
+/**
+ * Created by Jasper McNiven on 19/04/2018.
+ * Used to store data on the x, y, width and height of an object
+ */
+
+public class Bounds {
+
+    float x, y;
+    float width, height;
+
+    public Bounds(float x, float y, float width, float height)
+    {
+        this.x = x;
+        this.y = y;
+        this.width = width;
+        this.height = height;
+    }
+
+    public void SetBounds(float x, float y, float width, float height)
+    {
+        this.x = x;
+        this.y = y;
+        this.width = width;
+        this.height = height;
+    }
+
+    public float[] GetBounds()
+    {
+        float[] bounds = {x, y, width, height};
+        return bounds;
+    }
+
+    public float GetX(){ return x; }
+    public float GetY(){ return y; }
+    public float GetWidth(){ return width; }
+    public float getHeight(){ return height; }
+
+    public void SetX(float x){ this.x = x; }
+    public void SetY(float y){ this.y = y; }
+    public void SetWidth(float width){ this.width = width; }
+    public void SetHeight(float height){ this.height = height; }
+
+}
---
+++ app/src/main/java/com/example/comp2100/retrogame2018s1/GameObject.java
@@ -0,0 +1,32 @@
+package com.example.comp2100.retrogame2018s1;
+
+import android.content.Context;
+import android.graphics.Bitmap;
+import android.graphics.Canvas;
+import android.graphics.Rect;
+import android.support.annotation.Nullable;
+import android.util.AttributeSet;
+import android.view.View;
+
+/*
+    Created by Jasper McNiven, 19/04/2018
+    This class is the base class for all the visible objects in the game.
+    e.g. the player or obstacles
+ */
+
+public abstract class GameObject extends View
+{
+
+    Bounds bounds;
+    float xVel, yVel;
+
+    Bitmap image;
+
+    public GameObject(Context context, Bitmap image, Bounds bounds, @Nullable AttributeSet attrs) {
+        super(context, attrs);
+        this.image = image;
+        this.bounds = bounds;
+    }
+
+    protected abstract void OnDraw(Canvas canvas);
+}
