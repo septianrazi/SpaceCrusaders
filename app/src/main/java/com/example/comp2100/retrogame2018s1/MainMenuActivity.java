2745de96a2cbf16e1f5c64de66153109ede59252
Septian Razi <u6086829@anu.edu.au>
2018-04-19 16:23:55 +1000

    Added Game Activity

--- app/src/main/java/com/example/comp2100/retrogame2018s1/MainMenuActivity.java
+++ app/src/main/java/com/example/comp2100/retrogame2018s1/MainMenuActivity.java
@@ -1,8 +1,10 @@
 package com.example.comp2100.retrogame2018s1;
 
+import android.content.Intent;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
-
+import android.view.View;
+import android.widget.Button;
 
 
 public class MainMenuActivity extends AppCompatActivity {
@@ -10,6 +12,13 @@ public class MainMenuActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main_menu);
+
+        final Button button = findViewById(R.id.newGame);
+        button.setOnClickListener(new View.OnClickListener() {
+            public void onClick(View v) {
+                startActivity(new Intent(MainMenuActivity.this, GameActivity.class));
+            }
+        });
     }
 
 }
