7304fc668d129d1b6414b790fc8962e0d46529e3
Kriti Tripathi <u6058693@anu.edu.au>
2018-04-22 16:52:54 +1000

    added a pause menu button

--- app/src/main/java/com/example/comp2100/retrogame2018s1/GameActivity.java
+++ app/src/main/java/com/example/comp2100/retrogame2018s1/GameActivity.java
@@ -1,7 +1,10 @@
 package com.example.comp2100.retrogame2018s1;
 
+import android.content.Intent;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
 
 /*
     Created by Septian Razi, 19/04/2018
@@ -14,5 +17,12 @@ public class GameActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_game);
+
+        final Button pauseButton = findViewById(R.id.pauseGame);
+        pauseButton.setOnClickListener(new View.OnClickListener() {
+            public void onClick(View v) {
+
+            }
+        });
     }
 }
