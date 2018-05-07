f1346f7d33f8e7f41426ad0b14097e20878a5045
Kriti Tripathi <u6058693@anu.edu.au>
2018-05-07 19:18:37 +1000

    put an option for sound

--- app/src/main/java/com/example/comp2100/retrogame2018s1/GameActivity.java
+++ app/src/main/java/com/example/comp2100/retrogame2018s1/GameActivity.java
@@ -8,6 +8,7 @@ import android.widget.Button;
 
 /*
     Created by Septian Razi, 19/04/2018
+    Edited by Kriti Tripathi, 07/05/2018
     ACtivity for the Game to be played
  */
 
@@ -18,10 +19,28 @@ public class GameActivity extends AppCompatActivity {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_game);
 
-        final Button pauseButton = findViewById(R.id.btn_pauseGame);
-        pauseButton.setOnClickListener(new View.OnClickListener() {
+        final Button button_pause = findViewById(R.id.btn_pauseGame);
+        final Button button_help = findViewById(R.id.btn_help);
+        final Button button_quit = findViewById(R.id.btn_quit);
+
+        button_pause.setOnClickListener(new View.OnClickListener() {
             public void onClick(View v) {
 
+
+            }
+        });
+
+        button_help.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                startActivity(new Intent(GameActivity.this, InstructionsActivity.class));
+            }
+        });
+
+        button_quit.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                startActivity(new Intent(GameActivity.this, MainMenuActivity.class));
             }
         });
     }
