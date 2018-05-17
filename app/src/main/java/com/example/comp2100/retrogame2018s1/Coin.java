d9a1294c9fcb643938c636b8277470569fdf2549
Jasper <u6052551@anu.edu.au>
2018-05-17 17:23:03 +1000

    Finished the coin generation, displaying and collision

--- app/src/main/java/com/example/comp2100/retrogame2018s1/Coin.java
+++ app/src/main/java/com/example/comp2100/retrogame2018s1/Coin.java
@@ -4,6 +4,7 @@ import android.content.Context;
 import android.graphics.Canvas;
 import android.graphics.Color;
 import android.graphics.Paint;
+import android.graphics.Rect;
 import android.support.annotation.Nullable;
 import android.util.AttributeSet;
 
@@ -30,6 +31,12 @@ public class Coin extends GameObject {
     protected void OnDraw(Canvas canvas) {
         Paint paint = new Paint();
         paint.setColor(Color.RED);
-        canvas.drawCircle(bounds.GetX(), bounds.GetY(), bounds.getWidth() / 2, paint);
+        int left = (int) (bounds.GetX() - bounds.getWidth()/2);
+        int right = (int) (bounds.GetX() + bounds.getWidth()/2);
+        int top = (int) (bounds.GetY() - bounds.getHeight()/2);
+        int bottom = (int) (bounds.GetY() + bounds.getHeight()/2);
+        Rect rect = new Rect(left, top, right, bottom);
+        //canvas.drawCircle(bounds.GetX(), bounds.GetY(), bounds.getWidth() / 2, paint);
+        canvas.drawBitmap(CoinGenerator.coinImage, null, rect, null);
     }
 }
