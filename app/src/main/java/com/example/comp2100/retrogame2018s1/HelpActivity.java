a7f1437780be7d95931e80ae38a98228f96d4487
Kriti Tripathi <u6058693@anu.edu.au>
2018-05-17 18:40:01 +1000

    Made changes

--- app/src/main/java/com/example/comp2100/retrogame2018s1/HelpActivity.java
+++ app/src/main/java/com/example/comp2100/retrogame2018s1/HelpActivity.java
@@ -6,31 +6,26 @@ import android.support.v7.app.AppCompatActivity;
 import android.view.View;
 import android.widget.Button;
 
-/*
-    Created and edited by Kriti  Tripathi, 07/05/2018
-    Activity for the instructions of the game to be displayed */
-
-public class InstructionsActivity extends AppCompatActivity {
+/* This class was created and edited by Kriti Tripathi on 17/05/18
+* */
+public class HelpActivity extends AppCompatActivity {
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
-        setContentView(R.layout.activity_instructions);
+        setContentView(R.layout.activity_help);
         setRequestedOrientation(ActivityInfo.SCREEN_ORIENTATION_PORTRAIT);
 
         // Define the return button
-        final Button  button_return = findViewById(R.id.btn_helpResume);
+        final Button button_returnHelp = findViewById(R.id.btn_helpResume);
 
         // Implement it
-        button_return.setOnClickListener(new View.OnClickListener() {
+        button_returnHelp.setOnClickListener(new View.OnClickListener() {
             public void onClick(View v) {
-               System.exit(0);
+                System.exit(0);
             }
         });
 
 
     }
-
-
-
 }
